d3ea68de7d90c83c57eb46b5c13c45416f0b46de
cuong9000 <cuong13071988@gmail.com>
2020-03-02 00:21:46 +0700

    update

--- js/writerJS.js
+++ js/writerJS.js
@@ -17,6 +17,23 @@ $(document).ready(function(){
 		});
 	}
 	
+	if (window.matchMedia('(max-width: 768px)').matches)
+	{
+		$('#eclipse1').eclipse({
+						margin: 10,
+						slidesToShow: 1,
+						slidesToMove: 1
+                });
+	}
+	else 
+	{
+		$('#eclipse1').eclipse({
+				margin: 20,
+				slidesToShow: 4,
+				slidesToMove: 1
+		});
+	}
+	
 	
 	if (window.matchMedia('(max-width: 800px)').matches)
 	{
