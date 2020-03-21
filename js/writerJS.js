d79e4f02417bf829284773d2d419a185dbb3ee08
cuong9000 <cuong13071988@gmail.com>
2020-03-21 09:36:17 +0700

    Update writerJS.js

--- js/writerJS.js
+++ js/writerJS.js
@@ -35,21 +35,7 @@ $(document).ready(function(){
 	}
 	
 	
-	if (window.matchMedia('(max-width: 800px)').matches)
-	{
-		$('#eclipse5').eclipse({
-			slidesToShow: 3,
-			countIndex: 1,
-		});
-	}
-	else 
-	{
-		$('#eclipse5').eclipse({
-			margin: 50,
-			slidesToShow: 5,
-			countIndex: 2,
-		});
-	}
+	
 	
 });
 
